2107d16d1ff86ecf1a7b106378e63d936eca8b4d
hchalla2021 <53994609+hchalla2021@users.noreply.github.com>
2025-11-21 19:41:17 +0530

    Update test_facebook.py

--- Python_ParlellExecution/tests/test_facebook.py
+++ Python_ParlellExecution/tests/test_facebook.py
@@ -3,4 +3,4 @@ import pytest
 
 def test_facebook_chrome(chrome_driver):
     chrome_driver.get("https://www.facebook.com/")
-    assert "Facebook – log in or sign up" in chrome_driver.title
+    assert "Facebook – log in or sign up" in chrome_driver.title123
